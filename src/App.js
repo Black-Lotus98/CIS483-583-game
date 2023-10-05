a3001b79afba454493d963ea26e3eb805db0c122
saidions01 <101123304+saidions01@users.noreply.github.com>
2023-10-04 20:52:51 -0500

    update1.02

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Container from './components/Container/Container';
 function App() {
   return (
     <Fragment>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
+     
       <h1>Hello World</h1>
       <h1>Hello World</h1>
     </Fragment>
