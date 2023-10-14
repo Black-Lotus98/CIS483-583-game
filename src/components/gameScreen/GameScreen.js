28d622be93fa1a478f21dfc05cddf3debf62c31a
saidions01 <101123304+saidions01@users.noreply.github.com>
2023-10-13 19:03:02 -0500

    update

--- src/components/gameScreen/GameScreen.js
+++ src/components/gameScreen/GameScreen.js
@@ -1,88 +1,104 @@
-import React, { useState } from 'react'
-import IntroPage from './introPage/IntroPage'
-import TestPage from './testPage/TestPage';
-import MainMenu from './mainMenu/MainMenu';
-import MeatMarket from './meatMarket/MeatMarket';
-import Countries from './countries/Countries';
-import MeatRelease from './meatRelease/MeatRelease';
-import Taxes from './taxes/Taxes';
-import Map from './map/Map';
-import Scientists from './scientists/Scientists';
-
+import React, { useState } from "react";
+import IntroPage from "./introPage/IntroPage";
+import TestPage from "./testPage/TestPage";
+import MainMenu from "./mainMenu/MainMenu";
+import MeatMarket from "./meatMarket/MeatMarket";
+import Countries from "./countries/Countries";
+import MeatRelease from "./meatRelease/MeatRelease";
+import Taxes from "./taxes/Taxes";
+import Map from "./map/Map";
+import Scientists from "./scientists/Scientists";
+import BirthAndDeaths from "./BirthsAndDeaths/BirthAndDeaths";
 
 const GameScreen = (props) => {
-    const [currentScreen, setCurrentScreen] = useState('intro');
+  const [currentScreen, setCurrentScreen] = useState("intro");
 
-    const changeToTestPage = () => {
-        setCurrentScreen('test');
-    }
+  const changeToTestPage = () => {
+    setCurrentScreen("test");
+  };
 
-    const changeToIntroPage = () => {
-        setCurrentScreen('intro');
-    }
+  const changeToIntroPage = () => {
+    setCurrentScreen("intro");
+  };
 
-    const changeToMainMenu = () => {
-        setCurrentScreen('mainPage');
-    }
+  const changeToMainMenu = () => {
+    setCurrentScreen("mainPage");
+  };
 
-    const changeToCountryPage = () => {
-        setCurrentScreen('country');
-    }
+  const changeToCountryPage = () => {
+    setCurrentScreen("country");
+  };
 
-    const changeToMeatPage = () => {
-        setCurrentScreen('meatStore');
-    }
+  const changeToMeatPage = () => {
+    setCurrentScreen("meatStore");
+  };
 
+  const changeToMeatRelease = () => {
+    setCurrentScreen("meatRelease");
+  };
+  const changeToTaxes = () => {
+    setCurrentScreen("taxes");
+  };
 
-    const changeToMeatRelease = () => {
-        setCurrentScreen('meatRelease');
-    }
-    const changeToTaxes = () => {
-        setCurrentScreen('taxes');
-    }
+  const changeToMap = () => {
+    setCurrentScreen("map");
+  };
+  const changeToScientistReport = () => {
+    setCurrentScreen("scientistReport");
+  };
+  const changeToBirthsAndDeaths = () => {
+    setCurrentScreen("birthAndDeaths");
+  };
 
-    const changeToMap = () => {
-        setCurrentScreen('map');
-    }
-    const changeToScientistReport = () => {
-        setCurrentScreen('scientistReport');
-    }
-
-    let renderedScreen;
-    switch (currentScreen) {
-        case 'test':
-            renderedScreen = <TestPage changeToIntroPage={changeToIntroPage} changeToTaxes={changeToTaxes} changeToMap={changeToMap} />;
-            break;
-        case 'mainPage':
-            renderedScreen = <MainMenu changeToMeatRelease={changeToMeatRelease} changeToCountryPage={changeToCountryPage} changeToMeatPage={changeToMeatPage} />;
-            break;
-        case 'meatStore':
-            renderedScreen = <MeatMarket changeToMainMenu={changeToMainMenu} />;
-            break;
-        case 'country':
-            renderedScreen = <Countries changeToMainMenu={changeToMainMenu} />;
-            break;
-        case 'meatRelease':
-            renderedScreen = <MeatRelease changeToScientistReport={changeToScientistReport} />;
-            break;
-        case 'scientistReport':
-            renderedScreen = <Scientists changeToTestPage={changeToTestPage} />;
-            break;
-        case 'map':
-            renderedScreen = <Map changeToTestPage={changeToTestPage} />;
-            break;
-        case 'taxes':
-            renderedScreen = <Taxes changeToTestPage={changeToTestPage} />;
-            break;
-        default:
-            renderedScreen = <IntroPage changeToMainMenu={changeToMainMenu} />;
-
-    }
-    return (
-        <div className="gameGridScreen">
-            {renderedScreen}
-        </div>
-    )
-}
+  let renderedScreen;
+  switch (currentScreen) {
+    case "test":
+      renderedScreen = (
+        <TestPage
+          changeToIntroPage={changeToIntroPage}
+          changeToTaxes={changeToTaxes}
+          changeToMap={changeToMap}
+        />
+      );
+      break;
+    case "mainPage":
+      renderedScreen = (
+        <MainMenu
+          changeToMeatRelease={changeToMeatRelease}
+          changeToCountryPage={changeToCountryPage}
+          changeToMeatPage={changeToMeatPage}
+        />
+      );
+      break;
+    case "meatStore":
+      renderedScreen = <MeatMarket changeToMainMenu={changeToMainMenu} />;
+      break;
+    case "country":
+      renderedScreen = <Countries changeToMainMenu={changeToMainMenu} />;
+      break;
+    case "meatRelease":
+      renderedScreen = (
+        <MeatRelease changeToBirthsAndDeaths={changeToBirthsAndDeaths} />
+      );
+      break;
+    case "scientistReport":
+      renderedScreen = <Scientists changeToTestPage={changeToTestPage} />;
+      break;
+    case "map":
+      renderedScreen = <Map changeToTestPage={changeToTestPage} />;
+      break;
+    case "taxes":
+      renderedScreen = <Taxes changeToTestPage={changeToTestPage} />;
+      break;
+    case "birthAndDeaths":
+      renderedScreen = (
+        <BirthAndDeaths changeToScientistReport={changeToScientistReport} />
+      );
+      break;
+    default:
+      renderedScreen = <IntroPage changeToMainMenu={changeToMainMenu} />;
+  }
+  return <div className="gameGridScreen">{renderedScreen}</div>;
+};
 
 export default GameScreen;
--- src/components/gameScreen/meatRelease/MeatRelease.js
+++ src/components/gameScreen/meatRelease/MeatRelease.js
@@ -1,25 +1,33 @@
 import React, { Fragment, useState } from 'react'
 
 const MeatRelease = (props) => {
-    const [isChecked, setIsChecked] = useState(false);
+    const [inputText, setInputText] = useState(''); 
 
-    const handleCheckboxChange = () => {
-        setIsChecked(!isChecked);
-    };
+    const handleInputChange = (e) => {
+        setInputText(e.target.value);
+      };
 
     return (
         <Fragment>
-            <div id='content'>
-            <p id="text"> Sir of Santa Paravia <br/> you have 28500 st of grain<br/>
-            Grain demand is 11600 steres <br/> How much grain will you release for consumption </p>
-            <div id="input">
-            <input type="text" id="input-text"/>
-            <input type="checkbox" id="checkbox" onChange={handleCheckboxChange} />
-            {isChecked && (<button className='continue-btn' onClick={props.changeToScientistReport} >
-                Continue
-            </button>)}
-            </div>
-            </div>
+      <div id='content'>
+      <p id='name'>Sir of Santa Paravia</p>
+
+        <p id="text">You have 28500 kg of Meat<br/>
+          Meat demand is 11600 kg <br/> How much meat will you release for consumption knowing that you cannot give more than 20% of your stock  </p>
+        <div id="input">
+          <input
+            type="text"
+            id="input-text"
+            value={inputText}
+            onChange={handleInputChange}
+          />
+          {inputText && ( 
+            <button className='continue-btn' onClick={props.changeToBirthsAndDeaths}>
+              Continue
+            </button>
+          )}
+        </div>
+      </div>
             
         </Fragment>
     )
